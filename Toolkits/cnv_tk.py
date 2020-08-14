5311105b473e01919baa3926612ab3408975d526
Dylan Maxwell Kennedy <dmk324@mun.ca>
2020-08-14 11:54:56 -0230

    Added method StandardizedDF to cnv toolkit to create df using midlayer names for columns.
    Also Reuploaded Binned Writer

--- Toolkits/cnv_tk.py
+++ Toolkits/cnv_tk.py
@@ -844,3 +844,118 @@ def FetchCastObject(cast, conn):
 
 
 ###########################################################################################################
+
+# Creates dataframe using midlayer variables as the columns in order to standardize the column names
+def StandardizedDF(df):
+
+    col = df.columns._values
+    newdf = pd.DataFrame()
+
+    # for c in cast.ColumnNames:
+    for c in col:
+        name = c.replace("/", "+")
+        if c.lower().__eq__('prdm'):
+            newdf['Pressure'] = df[c].values
+
+        elif c.lower().__eq__('t090c'):
+            newdf['Temperature'] = df[c].values
+
+        elif c.lower().__eq__('t190c'):
+            newdf['Secondary Temperature'] = df[c].values
+
+        elif c.lower().__eq__('c0s/m'):
+            newdf['Conductivity'] = df[c].values
+
+        elif c.lower().__eq__('c1s/m'):
+            newdf['Secondary Conductivity'] = df[c].values
+
+        elif c.lower().__eq__('cond'):
+            newdf['Conductivity'] = df[c].values
+
+        elif c.lower().__eq__('cstarat0'):
+            newdf['Transmissometer attenuation'] = df[c].values
+
+        elif c.lower().__eq__('cstartr0'):
+            newdf['Transmissometer transmission'] = df[c].values
+
+        elif c.lower().__eq__('depth'):
+            newdf['Depth'] = df[c].values
+
+        elif c.lower().__eq__('flag'):
+            newdf['Flag'] = df[c].values
+
+        elif c.lower().__eq__('fleco-afl'):
+            newdf['Chlorophyll A Fluorescence'] = df[c].values
+
+        elif c.lower().__eq__('flor'):
+            newdf['Fluorescence'] = df[c].values
+
+        elif c.lower().__eq__('oxsatml/l'):
+            newdf['Oxygen Saturation'] = df[c].values
+
+        elif c.lower().__eq__('oxy'):
+            newdf['Oxygen Saturation'] = df[c].values
+
+        elif c.lower().__eq__('par'):
+            newdf['Irradiance'] = df[c].values
+
+        elif c.lower().__eq__('par/sat/log'):
+            newdf['Photosynthetic Active Radiation'] = df[c].values
+
+        elif c.lower().__eq__('ph'):
+            newdf['pH'] = df[c].values
+
+        elif c.lower().__eq__('pres'):
+            newdf['Pressure'] = df[c].values
+
+        elif c.lower().__eq__('sal'):
+            newdf['Salinity'] = df[c].values
+
+        elif c.lower().__eq__('sal00'):
+            newdf['Salinity'] = df[c].values
+
+        elif c.lower().__eq__('sal11'):
+            newdf['Secondary Salinity'] = df[c].values
+
+        elif c.lower().__eq__('sbeox0ml/l'):
+            newdf['Oxygen'] = df[c].values
+
+        elif c.lower().__eq__('sbeox0v'):
+            newdf['Oxygen Raw'] = df[c].values
+
+        elif c.lower().__eq__('sbeox1ml/l'):
+            newdf['Secondary Oxygen'] = df[c].values
+
+        elif c.lower().__eq__('sbeox1v'):
+            newdf['Secondary Oxygen Raw'] = df[c].values
+
+        elif c.lower().__eq__('scan'):
+            newdf['scan'] = df[c].values
+            continue
+            # Dictionary['Scan'] = [nc_out.createVariable('Scan', np.float32, ('level'), zlib=True, fill_value=-9999), name]
+
+        elif c.lower().__eq__('sigma-t00'):
+            newdf['Density'] = df[c].values
+
+        elif c.lower().__eq__('sigma-t11'):
+            newdf['Secondary Density'] = df[c].values
+
+        elif c.lower().__eq__('sigt'):
+            newdf['Density'] = df[c].values
+
+        elif c.lower().__eq__('temp'):
+            newdf['Temperature'] = df[c].values
+
+        elif c.lower().__eq__('wetcdom'):
+            newdf['CDOM Fluorescence'] = df[c].values
+
+        elif c.lower().__eq__('depsm'):
+            newdf['Depth'] = df[c].values
+
+        else:
+            newdf[c] = df[c].values
+            print("UNKNOWN VARIABLE: " + c.__str__())\
+
+    return newdf
+
+###########################################################################################################
