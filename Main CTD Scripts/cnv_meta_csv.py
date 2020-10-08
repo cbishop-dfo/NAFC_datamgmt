29ce49a68d0cc001ecdbddaf76630ef4af0b5112
Dylan Maxwell Kennedy <dmk324@mun.ca>
2020-10-08 16:47:36 -0230

    Changed cnv meta header to include ID and CruiseID also changed csv file names to match trips

--- Main CTD Scripts/cnv_meta_csv.py
+++ Main CTD Scripts/cnv_meta_csv.py
@@ -14,21 +14,23 @@ def writeMetaToCNV(cast, df):
     # TODO: Change file name to be unique ship+trip_META.csv
     fileExists = False
     for f in files:
-        if f == 'CNV_META.csv':
+        if f == csvName:
             fileExists = True
-    row = [datafile, cast.ship, cast.trip, cast.station, cast.Latitude, cast.Longitude, cast.CastDatetime, cast.Instrument, cast.SounderDepth, cast.SamplingRate, cast.comment]
+    id = cast.ship.__str__() + cast.trip.__str__() + cast.station.__str__()
+    cid = cast.ship.__str__() + cast.trip.__str__()
+    row = [datafile, id, cid, cast.ship, cast.trip, cast.station, cast.Latitude, cast.Longitude, cast.CastDatetime, cast.Instrument, cast.SounderDepth, cast.SamplingRate, cast.comment]
     if fileExists:
-        with open('CNV_META.csv', 'a', newline='') as csvfile:
+        with open(csvName, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(row)
             print(datafile)
 
     elif not fileExists:
-        with open('CNV_META.csv', 'w+', newline='') as csvfile:
+        with open(csvName, 'w+', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(["Filename", "Ship", "Trip", "Station", "Latitude", "Longitude", "Datetime", "SerialNumber", "Sounder Depth", "Sample Size", "Comments"])
+            writer.writerow(["Filename", "ID", "Cruise ID", "Ship", "Trip", "Station", "Latitude", "Longitude", "Datetime", "SerialNumber", "Sounder Depth", "Sample Size", "Comments"])
             writer.writerow(row)
-            files.append('CNV_META.csv')
+            files.append(csvName)
 
 
 
@@ -43,11 +45,15 @@ if __name__ == '__main__':
         os.chdir(dirName)
         datafile = f
         if datafile.lower().endswith(".cnv"):
-            print("Reading: " + datafile)
-            cast = cnv_tk.Cast(datafile)
-            cnv_tk.cnv_meta(cast, datafile)
-            df = cnv_tk.cnv_to_dataframe(cast)
-            writeMetaToCNV(cast,df)
+            try:
+                print("Reading: " + datafile)
+                cast = cnv_tk.Cast(datafile)
+                cnv_tk.cnv_meta(cast, datafile)
+                df = cnv_tk.cnv_to_dataframe(cast)
+                csvName = cast.ship.__str__() + cast.trip.__str__() + "_META.csv"
+                writeMetaToCNV(cast,df)
+            except Exception as e:
+                x = input("ERROR: " + e.__str__() + "\nProgram Halted!\nPress Enter To Continue...")
 
 
 
