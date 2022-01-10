14e4f8dbcc6e882a5e4db6d7deae76db44136b67
KENNEDYDYL <dmk324@mun.ca>
2022-01-10 13:08:21 -0330

    Test for BinnedNC

--- Main CTD Scripts/binnedNCWriter.py
+++ Main CTD Scripts/binnedNCWriter.py
@@ -1,7 +1,4 @@
 import sys
-sys.path.append('C://Users//dylan//PycharmProjects//Workspace//Git')
-# sys.path.append('C://Users//dylan//PycharmProjects//Workspace//Git//Toolkits')
-# sys.path.append('C://Users//dylan//PycharmProjects//Workspace//Git//Resources')
 from Toolkits import cnv_tk
 from Toolkits import dir_tk
 import os
@@ -104,253 +101,10 @@ def modifyDF(cast, df):
 
     return df
 
-
-def NCWrite(cast, df):
-    """
-    Takes Cast object and dataframe and creates a NETCDF using a standard naming scheme for the variables
-    Variable names are converted to a standard name from cast.ColumnNames which is populated during function call
-    df = cnv_tk.cnv_to_dataframe(cast)
-
-    To create and populate needed params:
-    cast = cnv_tk.Cast(datafile)        # Creates an empty Cast type object
-    cnv_tk.cnv_meta(cast, datafile)     # Populates meta variables and data arrays within the Cast object
-    df = cnv_tk.cnv_to_dataframe(cast)  # Creates and returns a pandas dataframe using data from cast
-
-    :param cast:
-    :param df:
-    :return:
-    """
-    ####################################################################################################
-    # NETCDF CREATION HERE:
-    ####################################################################################################
-    nc_outfile = cast.datafile.replace(".cnv", "").replace(".CNV", "") + "BINNED.nc"
-    nc_out = nc.Dataset(nc_outfile, 'w', format='NETCDF3_CLASSIC')
-    nc_out.Conventions = 'CF-1.6'
-    nc_out.title = 'AZMP netCDF file'
-    nc_out.institution = 'Northwest Atlantic Fisheries Centre, Fisheries and Oceans Canada'
-    nc_out.source = cast.datafile
-    nc_out.references = ''
-    nc_out.description = cast.comment
-    nc_out.created = 'Created ' + tt.ctime(tt.time())
-    # nc_out.processhistory = history
-    nc_out.trip_id = cast.id
-    nc_out.instrument_type = cast.InstrumentName
-    nc_out.instrument_ID = cast.Instrument
-    nc_out.shipname = cast.ShipName
-    nc_out.nafc_shipcode = cast.ship
-    nc_out.time_of_cast = cast.CastDatetime
-    nc_out.format_of_time = "YYYY-MM-DD HH:MM:SS"
-
-    history = []
-    # Here we are adding all header info into the history
-    for h in cast.header:
-        history.append(h.__str__().replace('\n', '').replace('(', '').replace(')', '').replace("'", ''))
-
-    num_history_lines = np.size(history)
-    # NOTE: ERROR when trying to exec only with map
-    # for i in range(0, num_history_lines):
-    # exec ('nc_out.processhistory_' + str(i) + ' = ' + ' \' ' + history[i].replace('\n','') + ' \' ')
-    # Create dimensions
-    time = nc_out.createDimension('time', 1)
-    # level = nc_out.createDimension('level', len(df_temp.columns))
-    level = nc_out.createDimension('level', df.shape[0])
-    # Create coordinate variables
-    times = nc_out.createVariable('time', np.float64, ('time',))
-    levels = nc_out.createVariable('level', np.float32, ('level',))
-    # **** NOTE: Maybe consider using ID instead of time for dimension **** #
-
-    # Create 1D variables
-    latitudes = nc_out.createVariable('latitude', np.float32, ('time'), zlib=True)
-    longitudes = nc_out.createVariable('longitude', np.float32, ('time'), zlib=True)
-    sounder_depths = nc_out.createVariable('sounder_depth', np.float32, ('time'), zlib=True)
-
-    Dictionary = {}
-    for c in cast.ColumnNames:
-        name = c.replace("/", "+")
-        if c.lower().__eq__('prdm'):
-            Dictionary['Pressure'] = [
-                nc_out.createVariable('Pressure', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('t090c'):
-            Dictionary['Temperature'] = [
-                nc_out.createVariable('Temperature', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('t190c'):
-            Dictionary['Secondary Temperature'] = [
-                nc_out.createVariable('Secondary Temperature', np.float32, ('level'), zlib=True, fill_value=-9999),
-                name]
-
-        elif c.lower().__eq__('c0s/m'):
-            Dictionary['Conductivity'] = [
-                nc_out.createVariable('Conductivity', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('c1s/m'):
-            Dictionary['Secondary Conductivity'] = [
-                nc_out.createVariable('Secondary Conductivity', np.float32, ('level'), zlib=True, fill_value=-9999),
-                name]
-
-        elif c.lower().__eq__('cond'):
-            Dictionary['Conductivity'] = [
-                nc_out.createVariable('Conductivity', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('cstarat0'):
-            Dictionary['Transmissometer attenuation [l per m]'] = [
-                nc_out.createVariable('Transmissometer attenuation [l per m]', np.float32, ('level'), zlib=True,
-                                      fill_value=-9999), name]
-
-        elif c.lower().__eq__('cstartr0'):
-            Dictionary['Transmissometer transmission [%]'] = [
-                nc_out.createVariable('Transmissometer transmission [%]', np.float32, ('level'), zlib=True,
-                                      fill_value=-9999), name]
-
-        elif c.lower().__eq__('depth'):
-            Dictionary['Depth'] = [nc_out.createVariable('Depth', np.float32, ('level'), zlib=True, fill_value=-9999),
-                                   name]
-
-        elif c.lower().__eq__('flag'):
-            Dictionary['Flag'] = [nc_out.createVariable('Flag', np.float32, ('level'), zlib=True, fill_value=-9999),
-                                  name]
-
-        elif c.lower().__eq__('fleco-afl'):
-            Dictionary['Chlorophyll A Fluorescence'] = [
-                nc_out.createVariable('Chlorophyll A Fluorescence', np.float32, ('level'), zlib=True, fill_value=-9999),
-                name]
-
-        elif c.lower().__eq__('flor'):
-            Dictionary['Fluorescence'] = [
-                nc_out.createVariable('Fluorescence', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('oxsatml/l'):
-            Dictionary['Oxygen Saturation'] = [
-                nc_out.createVariable('Oxygen Saturation', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('oxy'):
-            Dictionary['Oxygen Saturation'] = [
-                nc_out.createVariable('Oxygen Saturation', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('par'):
-            Dictionary['Irradiance'] = [
-                nc_out.createVariable('Irradiance', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('par/sat/log'):
-            Dictionary['Photosynthetic Active Radiation'] = [
-                nc_out.createVariable('Photosynthetic Active Radiation', np.float32, ('level'), zlib=True,
-                                      fill_value=-9999), name]
-
-        elif c.lower().__eq__('ph'):
-            Dictionary['pH'] = [nc_out.createVariable('pH', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('pres'):
-            Dictionary['Pressure'] = [
-                nc_out.createVariable('Pressure', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sal'):
-            Dictionary['Salinity'] = [
-                nc_out.createVariable('Salinity', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sal00'):
-            Dictionary['Salinity'] = [
-                nc_out.createVariable('Salinity', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sal11'):
-            Dictionary['Secondary Salinity'] = [
-                nc_out.createVariable('Secondary Salinity', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sbeox0ml/l'):
-            Dictionary['Oxygen'] = [nc_out.createVariable('Oxygen', np.float32, ('level'), zlib=True, fill_value=-9999),
-                                    name]
-
-        elif c.lower().__eq__('sbeox0v'):
-            Dictionary['Oxygen Raw'] = [
-                nc_out.createVariable('Oxygen Raw', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sbeox1ml/l'):
-            Dictionary['Secondary Oxygen'] = [
-                nc_out.createVariable('Secondary Oxygen', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sbeox1v'):
-            Dictionary['Secondary Oxygen Raw'] = [
-                nc_out.createVariable('Secondary Oxygen Raw', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('scan'):
-            continue
-            # Dictionary['Scan'] = [nc_out.createVariable('Scan', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sigma-t00'):
-            Dictionary['Density'] = [
-                nc_out.createVariable('Density', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sigma-t11'):
-            Dictionary['Secondary Density'] = [
-                nc_out.createVariable('Secondary Density', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('sigt'):
-            Dictionary['Density'] = [
-                nc_out.createVariable('Density', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('temp'):
-            Dictionary['Temperature'] = [
-                nc_out.createVariable('Temperature', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('wetcdom'):
-            Dictionary['CDOM Fluorescence'] = [
-                nc_out.createVariable('CDOM Fluorescence', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        elif c.lower().__eq__('tv290c'):
-            Dictionary['Temperature'] = [
-                nc_out.createVariable('Temperature', np.float32, ('level'), zlib=True, fill_value=-9999), name]
-
-        else:
-            Dictionary[c] = [nc_out.createVariable(name, np.float32, ('level'), zlib=True, fill_value=-9999),
-                             name]
-            print("UNKNOWN VARIABLE: " + c.__str__())
-            input("HALT...Press Enter To Continue")
-
-    times.units = 'hours since 1900-01-01 00:00:00'
-    times.calendar = 'gregorian'
-
-    # temp[:] = df["temp"].values
-    for d in Dictionary:
-        index = Dictionary[d][1]
-        index = index.replace("+", "/")
-        v = Dictionary[d][0]
-        v[:] = df[index].values
-        Dictionary[d][0][:] = df[index].values
-
-    # Fill cast info
-    latitudes[:] = cast.Latitude
-    longitudes[:] = cast.Longitude
-    sounder_depths[:] = str(cast.SounderDepth)
-    # Fill time
-    try:
-        date = datetime.datetime.strptime(cast.CastDatetime, '%Y-%m-%d %H:%M:%S')
-    except:
-        cast.CastDatetime = cast.CastDatetime + ":00"
-        date = datetime.datetime.strptime(cast.CastDatetime, '%Y-%m-%d %H:%M:%S')
-    times[:] = nc.date2num(date, units=times.units, calendar=times.calendar)
-
-    # Typically the pressure/depth index
-    pressureIndex = 1
-    for row in cast.InstrumentInfo:
-        if row.lower().__contains__("pressure") and not cast.isPressure:
-            # cast.isPressure = True
-            sRow = row.split(" ")
-            pressureIndex = int(sRow[2])
-            break
-        elif row.lower().__contains__("depth") and not cast.isPressure:
-            # cast.isPressure = True
-            cast.hasDepth = True
-            sRow = row.split(" ")
-            pressureIndex = int(sRow[2])
-            break
-    Pbin = np.array(df[cast.ColumnNames[pressureIndex]], dtype='float64')
-    levels[:] = Pbin
-    nc_out.close()
-
-
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     dirName = dir_path
+    errorArray = []
 
     #files = dir_tk.getListOfFiles(dirName)
     files = dir_tk.confirmSelection()
@@ -363,15 +117,27 @@ if __name__ == '__main__':
         except:
             datafile = f
         if datafile.lower().endswith(".cnv"):
-            print("Reading: " + datafile)
-            cast = cnv_tk.Cast(datafile)
-            cnv_tk.cnv_meta(cast, datafile)
-            df = cnv_tk.cnv_to_dataframe(cast)
-            # df = modifyDF(cast, df)
-            # df = cnv_tk.cnv_sig_dataframe(cast)
-            if isbinned.lower().__contains__("y"):
-                df = modifyDF(cast, df)
-            NCWrite(cast, df)
-            # input("Hold, Press enter to load next file")
+            try:
+                print("Reading: " + datafile)
+                cast = cnv_tk.Cast(datafile)
+                cnv_tk.cnv_meta(cast, datafile)
+                df = cnv_tk.cnv_to_dataframe(cast)
+                # df = modifyDF(cast, df)
+                # df = cnv_tk.cnv_sig_dataframe(cast)
+                if isbinned.lower().__contains__("y"):
+                    df = cnv_tk.StandardizedDF(cast,df)
+                    df = cnv_tk.BinDF(cast, df)
+                outfile = datafile.replace(".cnv", ".nc")
+                cnv_tk.NCWrite4(cast, df, nc_outfile=outfile)
+                # input("Hold, Press enter to load next file")
 
 
+            except Exception as e:
+                err = [e.__str__(), datafile]
+                errorArray.append(err)
+                continue
+
+    print("Script Complete\n")
+    print("*** List Of Errors ***")
+    for er in errorArray:
+        print("\nError: " + er[0] + "\nFilename: " + er[1])
