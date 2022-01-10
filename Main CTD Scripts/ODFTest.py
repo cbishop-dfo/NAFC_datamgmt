7d075ca44087bb88cb60f3dd687957519ce930d7
KENNEDYDYL <dmk324@mun.ca>
2022-01-10 13:06:57 -0330

    ODF to CNV tester file

--- Main CTD Scripts/ODFTest.py
+++ Main CTD Scripts/ODFTest.py
@@ -1,11 +1,12 @@
 from Toolkits import odf_tk
-from Toolkits import  cnv_tk
+from Toolkits import cnv_tk
 from Toolkits import dir_tk
 import os
 
 if __name__ == '__main__':
 
     files = dir_tk.confirmSelection()
+    errorArray = []
 
     for f in files:
         try:
@@ -13,10 +14,19 @@ if __name__ == '__main__':
         except:
             datafile = f
 
-        print("Reading: " + datafile)
-        cast = odf_tk.Cast(datafile)
-        odf_tk.odf_meta(cast, datafile)
-        df = odf_tk.odf_to_dataframe(cast)
+        try:
+            print("Reading: " + datafile)
+            cast = odf_tk.Cast(datafile)
+            odf_tk.odf_meta_artic(cast, datafile)
+            df = odf_tk.odf_to_dataframe(cast)
+            cnv_tk.cnv_write(cast, df, ext=".cnv")
 
+        except Exception as e:
+            err = [e.__str__(), datafile]
+            errorArray.append(err)
+            continue
 
-        cnv_tk.cnv_write(cast, df, ext=".odfTOcnv")
+    print("Script Complete\n")
+    print("*** List Of Errors ***")
+    for er in errorArray:
+        print("\nError: " + er[0] + "\nFilename: " + er[1])
