c7062bcfe88035f026b3a2da037bd1f1803c1c3a
KENNEDYDYL <dmk324@mun.ca>
2022-01-20 10:18:04 -0330

    Updated

--- CTD_AutoQA/RemoveDuplicates.py
+++ CTD_AutoQA/RemoveDuplicates.py
@@ -1,8 +1,8 @@
 exec(open("C:\QA_paths\set_QA_paths.py").read())
-
-from Toolkits import cnv_tk
 from Toolkits import dir_tk
 import pandas as pd
+import sys
+
 """Compare and remove duplicates from selected files"""
 
 if __name__ == '__main__':
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     print("Select File 1")
     File1 = dir_tk.selectSingleFile()
     File1DF = pd.read_csv(File1.name, delimiter="\n", header=None)
-
     print("Select File 2")
     File2 = dir_tk.selectSingleFile()
     File2DF = pd.read_csv(File2.name, delimiter="\n", header=None)
@@ -25,18 +24,36 @@ if __name__ == '__main__':
     File3 = dir_tk.selectSingleFile()
     File3DF = pd.read_csv(File3.name, delimiter="\n", header=None)
 
-    for i in MainFile:
-        if i in File1:
-            continue
+    for i in MainFileDF.values:
+        match = False
+        for j in File1DF.values:
+            x = i[0]
+            y = j[0]
+            if x == y:
+                match = True
+                print("Match")
 
-        elif i in File2:
-            continue
+        for k in File2DF.values:
+            x = i[0]
+            y = k[0]
+            if x == y:
+                match = True
+                print("Match")
 
-        elif i in File3:
-            continue
+        for l in File3DF.values:
+            x = i[0]
+            y = l[0]
+            if x == y:
+                match = True
+                print("Match")
 
-        else:
-            Remaining.append("i")
+        if match == False:
+            Remaining.append(i[0])
+            print(len(Remaining))
             # TODO: Write out Remaining files
 
     print("Duplicates Removed.")
+    with open("RemainingFiles.dat", "w") as f:
+        for line in Remaining:
+            f.write(line + "\n")
+    f.close()
